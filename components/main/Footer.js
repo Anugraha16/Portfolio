f937e0863a72c844e99e7ecf52d1aa4687d20860
Anugraha16 <anugrahaaa1216@gmail.com>
2024-08-30 20:22:34 +0530

    second  commit

--- components/main/Footer.js
+++ components/main/Footer.js
@@ -4,16 +4,16 @@ import { FaEnvelope, FaPhoneAlt } from "react-icons/fa";
 const Footer = () => {
   return (
     <section id="about-me">
-      <footer className="w-full text-gray-200 py-6 flex justify-between items-center px-4">
+      <footer className="w-full text-gray-200 py-6 px-4 flex flex-col sm:flex-row justify-between items-center">
         {/* Left Section: Copyright */}
-        <div className="text-gray-400 text-base">
+        <div className="text-gray-400 text-base mb-4 sm:mb-0">
           &copy; {new Date().getFullYear()} Anugraha. All rights reserved.
         </div>
 
         {/* Center Section: Email */}
         <a
           href="mailto:mifwebchain@gmail.com"
-          className="flex items-center text-gray-400 hover:text-white transition-colors duration-300 text-lg"
+          className="flex items-center text-gray-400 hover:text-white transition-colors duration-300 text-lg mb-4 sm:mb-0"
         >
           <FaEnvelope className="w-6 h-6 mr-2" />
           <span className="text-lg">anugrahaaa1216@gmail.com</span>
